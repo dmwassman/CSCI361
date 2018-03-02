a3a156b0e1c3d1d533684b8c9b4183f78f7504e7
dmwassman <dmwassman@gmail.com>
2018-03-01 20:32:36 -0500

    initial commit

---
+++ DavidWassmanMoney.h
@@ -0,0 +1,57 @@
+//Header file Money.h: This is the INTERFACE for the class Money
+//Values of this type are dollars and cents which are converted to total cents.
+//Input and output are in (-)$0.00 format
+
+#ifndef MONEY_H
+#define MONEY_H
+
+class Money{
+	public:
+		//Constructors
+		Money(long dollars, int cents);
+		Money(long dollars);
+		Money();
+		
+		//friend functions
+		friend Money operator +(const Money& amount1, const Money& amount2);
+		//Precondition: amount1 and amount2 have been given values.
+		//Postcondition: returns amount1 + amount 2
+		friend Money operator -(const Money& amount1, const Money& amount2);
+		//Precondition: amount1 and amount2 have been given values.
+		//Postcondition: returns amount1 - amount2
+		friend bool operator ==(const Money& amount1, const Money& amount2);
+		//Precondition: amount1 and amount2 have been given values
+		//Postcondition: Returns true if amount1 = amount2, false otherwise
+		friend bool operator <(const Money& amount1, const Money& amount2);
+		//Precondition: amount1 and amount2 have been given values
+		//Postcondition: Returns true if amount1 < amount2, false otherwise
+		friend bool operator <=(const Money& amount1, const Money& amount2);
+		//Precondition: amount1 and amount2 have been given values
+		//Postcondition: Returns true if amount <= amount2, false otherwise
+		friend bool operator >(const Money& amount1, const Money& amount2);
+		//Precondition: amount1 and amount2 have been given values
+		//Postcondition: Returns true if amount1 > amount2, false otherwise
+		friend bool operator >=(const Money& amount1, const Money& amount2);
+		//Precondition: amount1 and amount2 have been given values
+		//Postcondition: Returns true if amount >= amount2, false otherwise
+		friend istream& operator >>(istream& ins, Money& amount);
+		//Postcondition: Add value to amount in proper format
+		friend ostream& operator <<(ostream& outs, const Money& amount);
+		//Precondition: amount has been given a value
+		//Postcondition: display amount in proper format
+	
+		//member functions
+		void operator -();
+		//Precondition: class object declared and given value
+		//Postcondition: Returns negative of the value in class object
+		double getValue() const;
+		//Precondition: class object initialized
+		//Postcondition: return value of allCents
+		Money percent(int percentFigure) const;
+		//Precondition: class object declared and initialized
+		//Postcondition: returns 10% of the amount in class object
+	private:
+		long allCents;
+};
+
+#endif
